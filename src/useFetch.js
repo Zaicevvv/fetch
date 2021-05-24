01cf761f4b72e37ba39d6333372b46faaf64bd8d
ZaitsevVV <66888899+ZaitsevVV@users.noreply.github.com>
2021-05-24 17:15:23 +0300

    isMounted check

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,19 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios'
 
 function useFetch(url, foo) {
-const [data, setData] = useState(null)
-const [error, setError] = useState(null)
+const [data, setData] = useState(null);
+const [error, setError] = useState(null);
+const isMounted = useRef(false)
 
 useEffect(() => {
-axios.get(url)
- .then(res => {
+  isMounted.current = true;
+  const source = axios.CancelToken.source();
+
+  axios.get(url, { cancelToken: source.token })
+   .then(res => {
+     if (!isMounted.current) return;
      res.data.content && foo(res.data.content) && setData(res.data.content);
      res.content && foo(res.content) && setData(res.content);
- })
- .catch(e => setError(e.message))},[])
+   })
+   .catch(e => setError(e.message))
+
+  return () => {
+    source.cancel();
+    isMounted.current = false;
+  }
+ 
+},[url, foo]);
 
   return { data, error }
-  };
+};
 
 export default useFetch;
